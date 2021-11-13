694fd10fadfb264e78c3ba58b0cf0f75208b3670
ricvolpe <volpatorv@gmail.com>
2021-11-13 17:17:42 +0000

    Load all answers at once

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,94 +1,95 @@
 import { getUser } from '../api/twitter'
+import { increaseScore } from '../redux/scoreSlice';
+import { setAnswerIDs } from "../redux/answerOptionsSlice"
+import { useHistory } from "react-router-dom";
+import { Box, Flex } from 'reflexbox'
 import { useEffect, useState } from "react"
 import { useSelector, useDispatch } from "react-redux";
-import { setAnswerIDs } from "../redux/answerOptionsSlice"
-import { increaseScore } from '../redux/scoreSlice';
-import { Box, Flex } from 'reflexbox'
-import { useHistory } from "react-router-dom";
 
 export default function Answers(props) {
-    const dispatch = useDispatch()
-    const friendsIds = useSelector((state) => state.friendsIds.value)
-    const [users, setUsers] = useState(null)
-    const [answer, giveAnswer] = useState(null)
-    const history = useHistory();
+  const history = useHistory();
+  const dispatch = useDispatch()
+  const friendsIds = useSelector((state) => state.friendsIds.value)
+  const [users, setUsers] = useState(null)
+  const [answer, giveAnswer] = useState(null)
 
-    useEffect(() => {
-      async function getAnswers(friendsIds) {
-        if (friendsIds.length === undefined) {
-          return null
-        }
-        const shuffledFriendsIds = shuffle(friendsIds)
-        var answerUserIDs = []
-        var answerUsers = []
-        var unfetchableUsers = []
-        while (answerUserIDs.length < 4) {
-          const possibleID = shuffledFriendsIds[Math.floor(Math.random() * friendsIds.length)]
-          if (possibleID && !answerUserIDs.includes(possibleID) && !unfetchableUsers.includes(possibleID)) {
-            const resp = await getUser(possibleID)
-            if (resp['statusCode'] === 200) {
-              answerUserIDs = [...answerUserIDs, possibleID]
-              answerUsers = [...answerUsers, resp.data]
-            } else {
-              console.log('Could not fetch user for answer', possibleID, 'HTTP status code', resp.statusCode)
-              unfetchableUsers = [...unfetchableUsers, possibleID]
-            }
+  useEffect(() => {
+    async function getAnswers(friendsIds) {
+      if (friendsIds.length === undefined) {
+        return null
+      }
+      const shuffledFriendsIds = shuffle(friendsIds)
+      var answerUserIDs = []
+      var answerUsers = []
+      var unfetchableUsers = []
+      while (answerUserIDs.length < 4) {
+        const possibleID = shuffledFriendsIds[Math.floor(Math.random() * friendsIds.length)]
+        if (possibleID && !answerUserIDs.includes(possibleID) && !unfetchableUsers.includes(possibleID)) {
+          const resp = await getUser(possibleID)
+          if (resp['statusCode'] === 200) {
+            answerUserIDs = [...answerUserIDs, possibleID]
+            answerUsers = [...answerUsers, resp.data]
+          } else {
+            console.log('Could not fetch user for answer', possibleID, 'HTTP status code', resp.statusCode)
+            unfetchableUsers = [...unfetchableUsers, possibleID]
           }
-        } 
-        dispatch(setAnswerIDs(answerUserIDs))
-        setUsers(answerUsers)
-      }
-      giveAnswer(null)
-      getAnswers(getAnswers(friendsIds))
-    }, [friendsIds, dispatch, history.location])
-    
-    const { tweetAuthor } = props;
-
-    const answerQuestion = (selectedUserID) => {
-      if (answer === null) {
-        giveAnswer(selectedUserID)
-        if (selectedUserID === tweetAuthor.id) {
-          dispatch(increaseScore(1))
-        } 
-      }
+        }
+      } 
+      dispatch(setAnswerIDs(answerUserIDs))
+      setUsers(answerUsers)
     }
+    setUsers([])
+    giveAnswer(null)
+    getAnswers(getAnswers(friendsIds))
+  }, [friendsIds, dispatch, history.location])
+  
+  const { tweetAuthor } = props;
 
-    if (users?.length > 0 && tweetAuthor) {
-      const uniqueUsers = users.filter(u => u[0].id !== tweetAuthor.id).slice(0, 3)
-      const positionDecider = (uniqueUsers[0][0].id + tweetAuthor.id) % 4
-      const fullAnswers = [...uniqueUsers.slice(0, positionDecider), [tweetAuthor], ...uniqueUsers.slice(positionDecider)]
-      return (
-        <Flex className="grid" flexWrap='wrap' sx={{marginTop: '100px', justifyContent: 'center'}}>
-          {fullAnswers.map(u => {
-            return (
-              <Box 
-                key={u[0].id}
-                width={'100px'} height={'100px'} 
-                onClick={() => answerQuestion(u[0].id)}
-                sx={{
-                  backgroundColor: answer !== null? (u[0].id === tweetAuthor.id? 'green': 'red') : 'white',
-                  justifyContent: 'center', 
-                  cursor: 'pointer',
-                  marginLeft: '30px'}}
-                >
-                <Box sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center'
-                }}>
-                {u[0].profile_image_url_https? <img alt={`Avatar of Twitter user ${u[0].screen_name}`} className='AnswerAvatar' src={u[0].profile_image_url_https.replace('_normal', '_bigger')} /> : null}
-                </Box>
-                <Box>
-                  <p className='AnswerName'>{u[0].name}</p>  
-                </Box>
+  const answerQuestion = (selectedUserID) => {
+    if (answer === null) {
+      giveAnswer(selectedUserID)
+      if (selectedUserID === tweetAuthor.id) {
+        dispatch(increaseScore(1))
+      } 
+    }
+  }
+
+  if (users?.length > 0 && tweetAuthor) {
+    const uniqueUsers = users.filter(u => u[0].id !== tweetAuthor.id).slice(0, 3)
+    const positionDecider = (uniqueUsers[0][0].id + tweetAuthor.id) % 4
+    const fullAnswers = [...uniqueUsers.slice(0, positionDecider), [tweetAuthor], ...uniqueUsers.slice(positionDecider)]
+    return (
+      <Flex className="grid" flexWrap='wrap' sx={{marginTop: '100px', justifyContent: 'center'}}>
+        {fullAnswers.map(u => {
+          return (
+            <Box 
+              key={u[0].id}
+              width={'100px'} height={'100px'} 
+              onClick={() => answerQuestion(u[0].id)}
+              sx={{
+                backgroundColor: answer !== null? (u[0].id === tweetAuthor.id? 'green': 'red') : 'white',
+                justifyContent: 'center', 
+                cursor: 'pointer',
+                marginLeft: '30px'}}
+              >
+              <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+              }}>
+              {u[0].profile_image_url_https? <img alt={`Avatar of Twitter user ${u[0].screen_name}`} className='AnswerAvatar' src={u[0].profile_image_url_https.replace('_normal', '_bigger')} /> : null}
               </Box>
-            )
-          })}
-        </Flex>
-      )
-    } else {
-      return null
-    }
+              <Box>
+                <p className='AnswerName'>{u[0].name}</p>  
+              </Box>
+            </Box>
+          )
+        })}
+      </Flex>
+    )
+  } else {
+    return null
+  }
 }
 
 
